e22cf4d0b97eb5a8f491d692a8528952d98afc58
Soyn <w604408482@163.com>
2019-12-14 16:19:58 +0800

    1. reuse async function 2. add navigator

--- src/Movie.jsx
+++ src/Movie.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import "./movies.scss";
 
+const mockRequestResource = (url, time, resolve, reject) => {
+  setTimeout(() => {
+    window.fetch(url).then(res => {
+      resolve(res);
+    }, reject);
+  }, time);
+};
+const fetchImg = (url, resolve) => {
+  mockRequestResource(url, 3000, resolve);
+};
+const fetchMovie = (url, resolve) => {
+  mockRequestResource(url, 2000, res => {
+    res.json().then(resolve);
+  });
+};
 class Image extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       url: props.src,
+      name: props.name,
       loaded: false
     };
   }
@@ -15,7 +31,7 @@ class Image extends React.Component {
         <img
           src={this.state.url}
           className="movie-image"
-          alt={this.state.alt}
+          alt={this.state.name}
         />
       );
     }
@@ -23,13 +39,10 @@ class Image extends React.Component {
   }
 
   componentDidMount() {
-    window.fetch(this.state.url).then(img => {
-      console.log("img ", img);
-      setTimeout(() => {
-        this.setState({
-          loaded: true
-        });
-      }, 2000);
+    fetchImg(this.state.url, img => {
+      this.setState({
+        loaded: true
+      });
     });
   }
 }
@@ -38,11 +51,49 @@ class Description extends React.Component {
     super(props);
     this.state = {
       content: props.content,
+      title: props.title,
       loaded: false
     };
   }
   render() {
-    return <p className="movie-description">{this.state.content}</p>;
+    return (
+      <>
+        <div className="movie-title">{this.state.title}</div>
+        <p className="movie-description">{this.state.content}</p>
+      </>
+    );
+  }
+}
+class Navigator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      previous: -1,
+      current: 0,
+      loaded: false,
+    }
+  }
+  jumpToPrevious = () => {
+    this.setState({
+      current: this.state.current - 1,
+      previous: this.state - 2,
+    }, () => {
+      this.props.onChange(this.state.current);
+    })
+  }
+  jumpToNext = () => {
+    this.setState({
+      previous: this.state.current,
+      current: this.state.current + 1,
+    }, () => {
+      this.props.onChange(this.state.current);
+    })
+  }
+  render() {
+    return <div className="navigator-container">
+      {this.state.previous > - 1 && <button onClick={this.jumpToPrevious}>{"<-- Previous"}</button>}
+      {<button onClick={this.jumpToNext}>{"Next --->"}</button>}
+    </div>;
   }
 }
 class Movie extends React.Component {
@@ -53,33 +104,29 @@ class Movie extends React.Component {
       movies: []
     };
   }
+  switchMovie = (idx) => {
+    
+  }
   render() {
     if (this.state.movies.length) {
+      const movieInfo = this.state.movies[this.state.articleId];
       return (
         <div className="movie-review-container">
-          <Image src={this.state.movies[this.state.articleId].image} />
-          <Description
-            content={this.state.movies[this.state.articleId].description}
-          />
+          <Image src={movieInfo.image} name={movieInfo.name} />
+          <Description content={movieInfo.description} title={movieInfo.name} />
+          <Navigator onChange={this.switchMovie}/>
         </div>
       );
     }
     return <div>Loading...</div>;
   }
   componentDidMount() {
-    window
-      .fetch("data/moviesReview.json")
-      .then(res => res.json())
-      .then(data => {
-        const movies = data.movies;
-        setTimeout(() => {
-          console.log(movies);
-          this.setState({
-            movies
-          });
-        }, 3000);
+    fetchMovie("data/moviesReview.json", data => {
+      const movies = data.movies;
+      this.setState({
+        movies
       });
+    });
   }
 }
-
 export default Movie;
